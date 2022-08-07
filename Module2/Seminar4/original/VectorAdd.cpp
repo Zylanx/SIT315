a18b0f7b9455e6f437c8337160b35a1ca5f1e297
Zylanx <zylanxdragon@gmail.com>
2022-08-07 18:01:31 +1000

    Add Seminar 4

---
+++ Module2/Seminar4/original/VectorAdd.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <cstdlib>
+#include <time.h>
+#include <chrono>
+
+
+using namespace std::chrono;
+using namespace std;
+
+void randomVector(int vector[], int size)
+{
+    // Loop through the vector, generating a random integer between 0 and 100 for each slot.
+    for (int i = 0; i < size; i++)
+    {
+        vector[i] = rand() % 100;
+    }
+}
+
+
+int main(){
+
+    unsigned long size = 100000000;
+
+    srand(time(0));
+
+    int *v1, *v2, *v3;
+
+    // Store the time before the execution of the algorithm, for computing run time
+    auto start = high_resolution_clock::now();
+
+    // Allocate memory for the vectors
+    v1 = (int *) malloc(size * sizeof(int *));
+    v2 = (int *) malloc(size * sizeof(int *));
+    v3 = (int *) malloc(size * sizeof(int *));
+
+
+    randomVector(v1, size);
+
+    randomVector(v2, size);
+
+
+    // Compute the vector addition for each element of the input vectors
+    for (int i = 0; i < size; i++)
+    {
+        v3[i] = v1[i] + v2[i];
+    }
+
+    auto stop = high_resolution_clock::now();
+
+    // Compute the run time of the algorithm
+    auto duration = duration_cast<microseconds>(stop - start);
+
+
+    cout << "Time taken by function: "
+         << duration.count() << " microseconds" << endl;
+
+    return 0;
+}
