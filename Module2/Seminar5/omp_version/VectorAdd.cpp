1f77d04d9bb6610769029e3ab750f8161a5d0095
Zylanx <zylanxdragon@gmail.com>
2022-08-10 09:43:41 +1000

    Update Seminar 5

--- Module2/Seminar5/omp_version/VectorAdd.cpp
+++ Module2/Seminar5/omp_version/VectorAdd.cpp
@@ -45,52 +45,59 @@ int main()
 
     omp_set_num_threads(omp_get_max_threads());
 
+    // Randomise the input arrays in parallel.
+    // Both can be done at the same time, so ensure the for loops
+    // have the nowait clause to remove the barriers between them.
 #pragma omp parallel default(none) shared(size, v1, v2)
     {
-        // Randomise the input vectors data.
+        // Create a private initial seed for each thread
         unsigned int seed = time(nullptr) * (omp_get_thread_num() + 1);
 
-#pragma omp for nowait schedule(runtime)
+        // Randomise Vector 1
+#pragma omp for nowait
         for (int i = 0; i < size; i++)
         {
             v1[i] = rand_r(&seed) % 100;
         }
 
-#pragma omp for schedule(runtime)
+        // Randomise Vector 2
+#pragma omp for nowait
         for (int i = 0; i < size; i++)
         {
             v2[i] = rand_r(&seed) % 100;
         }
     }
 
-#pragma omp parallel default(none) shared(size, v1, v2, v3)
+    // Do the vector addition in parallel
+#pragma omp parallel for default(none) shared(size, v1, v2, v3)
     {
         // Compute the vector addition for each element of the input vectors
-#pragma omp for schedule(runtime)
         for (int i = 0; i < size; i++)
         {
             v3[i] = v1[i] + v2[i];
         }
     }
 
+    // Calculate the total using a reduction clause
     int total;
 #pragma omp parallel default(none) shared(size, v3, total)
     {
-#pragma omp for reduction(+:total) schedule(runtime)
+#pragma omp for reduction(+:total)
         for (int i = 0; i < size; i++)
         {
             total += v3[i];
         }
     };
-
     cout << "Total 1: " << total << endl;
 
+    // Calculate the total using a parallel for loop then a
+    // critical section to bring all the data together.
     int total2;
 #pragma omp parallel default(none) shared(total2, size, v3)
     {
         int privateTotal;
 
-#pragma omp for schedule(runtime)
+#pragma omp for
         for (int i = 0; i < size; i++)
         {
             privateTotal += v3[i];
@@ -101,7 +108,6 @@ int main()
             total2 += privateTotal;
         };
     }
-
     cout << "Total 2: " << total2 << endl;
 
     // Store the time after the execution of the algorithm
@@ -110,7 +116,7 @@ int main()
     // Compute the run time of the algorithm
     auto duration = duration_cast<microseconds>(stop - start);
 
-    // write the first 100 elements of the vectors
+    // Display the first 100 elements of the vectors
     cout <<"v1: \n\n  ";
     printArray(v1, size < 100 ? size : 100);
     cout <<"v2: \n\n  ";
